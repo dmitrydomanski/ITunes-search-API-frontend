205fff4fe127fbed5a7fa9f235e9ab548fccdd1d
Dmitry Domanski <dmitry.domanski@gmail.com>
2018-11-18 02:12:07 +0200

    routing implemented

--- src/components/hoc/Layout/index.jsx
+++ src/components/hoc/Layout/index.jsx
@@ -1,122 +1,44 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
+// import PropTypes from 'prop-types';
+import { Route, Switch, withRouter } from 'react-router-dom';
 
 import Aux from '../Aux';
-import SearchInput from '../../UI/SearchInput';
-import SearchResultItem from '../../SearchResultItem';
-import Modal from '../../UI/Modal';
-import Loader from '../../UI/Loader';
-import Header from '../../UI/Header';
-import Section from '../../UI/Section';
-import Informer from '../../UI/Informer';
-import Button from '../../UI/Button';
+import ScreenOne from '../../../containers/ScreenOne';
 
-const url = 'http://localhost:3000/api/search?term=';
+const layout = () => (
+    <Aux>
+        <Switch>
+            <Route
+                exact
+                path="/"
+                component={ScreenOne}
+            />
+            {/* <Route
+                path="/treechart"
+                component={TreeChart}
+            />
+            <Route
+                path="/gallery"
+                component={DisclaimerPage}
+            />
+            <Route
+                path="/documents"
+                component={DisclaimerPage}
+            /> */}
+        </Switch>
+    </Aux>
+);
 
-class Layout extends Component {
-    state = {
-        results: [],
-        isLoaded: true,
-        dataSearched: false,
-        dataSaved: false,
-        popQueries: [],
-    }
+// layout.propTypes = {
+//     location: PropTypes.shape({
+//         pathname: PropTypes.string.isRequired,
+//     }),
+// };
 
-    getSearchItems = async (query) => {
-        const encodedValue = encodeURIComponent(query);
-        await axios.get(`${url}${encodedValue}`)
-            .then((response) => {
-                this.setState({
-                    results: response.data,
-                    isLoaded: true,
-                    dataSearched: true,
-                    popQueries: [],
-                });
-            });
-    }
+// layout.defaultProps = {
+//     location: {
+//         pathname: '',
+//     },
+// };
 
-    saveSearchItem = async (query) => {
-        const encodedValue = encodeURIComponent(query);
-        await axios.post(`${url}${encodedValue}`)
-            .then((response) => {
-                if (response.status === 500) {
-                    this.setState({
-                        dataSaved: false,
-                    });
-                }
-                if (response.status === 201) {
-                    this.setState({
-                        dataSaved: true,
-                    });
-                }
-            });
-    }
-
-    getPopularRequests = async () => {
-        this.setState({
-            isLoaded: false,
-        });
-        await axios.get('http://localhost:3000/api/top')
-            .then((response) => {
-                this.setState({
-                    popQueries: response.data,
-                    isLoaded: true,
-                    results: [],
-                    dataSearched: false,
-                });
-            });
-    }
-
-    performSearch = (query) => {
-        this.setState({
-            isLoaded: false,
-        });
-        this.getSearchItems(query);
-        this.saveSearchItem(query);
-    }
-
-    render() {
-        const { isLoaded, results, dataSearched, dataSaved, popQueries } = this.state;
-        const informer = 'Sorry, there are no such videos on iTunes, try another search please';
-        if (!isLoaded) {
-            return (
-                <Modal show={!isLoaded}>
-                    <Loader />
-                </Modal>
-            );
-        }
-        return (
-            <Aux>
-                <Header title="Search and view music videos from iTunes">
-                    <SearchInput onSearch={this.performSearch} />
-                    {dataSearched && !dataSaved
-                        ? <div>query not saved</div>
-                        : null}
-                    <Button
-                        clicked={this.getPopularRequests}
-                    >Get 10 most popular requests
-                    </Button>
-                </Header>
-                <Section>
-                    {dataSearched && results.length === 0
-                        ? <Informer title={informer} />
-                        : results.map((item, index) => (
-                            <SearchResultItem
-                                artist={item.artistName}
-                                track={item.trackName}
-                                picture={item.artworkUrl100}
-                                key={parseInt(index.toString(), 10)}
-                            />
-                        ))}
-                    {popQueries.length === 0
-                        ? null
-                        : popQueries.map((item, index) => (
-                            <div key={parseInt(index.toString(), 10)}>{item}</div>
-                        ))}
-                </Section>
-            </Aux>
-        );
-    }
-}
-
-export default Layout;
+export default withRouter(layout);
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/app';
 
 const container = document.createElement('div');
 document.body.appendChild(container);
 
-ReactDom.render(<App />, container);
+ReactDom.render(
+    (
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>),
+    container,
+);
